a0e110d155492b2b69458470deac09489b345546
verengan122 <Crysis00!>
2022-05-23 20:17:32 +0300

    ready

--- src/test/java/config/EmulationConfig.java
+++ src/test/java/config/EmulationConfig.java
@@ -2,7 +2,7 @@ package config;
 
 import org.aeonbits.owner.Config;
 
-@Config.Sources({"classpath:emulattion.properties"})
+@Config.Sources({"classpath:emulation.properties"})
 public interface EmulationConfig extends Config {
     @Config.Key("appPackage")
     String appPackage();
